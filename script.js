93de669ff8b7bcd6e716985c6853cd20bd46a261
cahyaravii <jongjava234@gmail.com>
2025-08-26 21:05:31 +0700

    new version html css js

--- script.js
+++ script.js
@@ -1,113 +1,272 @@
-//Data Buku
+// // //Data Buku
+// // const books = [
+//     { 
+//         title: "Dasar Pemrograman Python", 
+//         link: "https://drive.google.com/file/d/1R1HLJrrW5B6YOegFDQTejZ20SFIedPfN/view?usp=sharing", 
+//         thumbnail: "img/dasarpemrogramanpython.jpg"
+//     },
+//     { 
+//         title: "Computer Graphics", 
+//         link: "https://drive.google.com/file/d/1pMHf74OUJv_0RAiLtqUlLUNVWejZm0kj/view?usp=sharing", 
+//         thumbnail: "img/Computergraphics.jpg"
+//     },
+//     { 
+//         title: "Belajar MS WOrd 2012", 
+//         link: "https://drive.google.com/file/d/1zWj3C3gSXtyy15IAo3W3vxQdZFpDWuxj/view?usp=sharing", 
+//         thumbnail: "img/belajarmsword2010.jpg"
+//     },
+//     { 
+//         title: "Panduan Merakit Komputer", 
+//         link: "https://drive.google.com/file/d/1N12HOhNo5RLlBff6VH8n6q2Z0A6sXqLM/view?usp=sharing", 
+//         thumbnail: "img/bukumerakitkomputer.jpg"
+//     },
+//     { 
+//         title: "Analysis and Design Using MATLAB", 
+//         link: "https://drive.google.com/file/d/16-wrtZjN27V0Taqvjg6xGG8PIIB4twKh/view?usp=sharing", 
+//         thumbnail: "img/designmatlab.jpg"
+//     },
+//     { 
+//         title: "Bikin Game Anti Ribet", 
+//         link: "https://drive.google.com/file/d/1XgRaa5lz68DpDLtHaaOJxuYEL4__69f-/view?usp=sharing", 
+//         thumbnail: "img/bikingameantiribet.jpg"
+//     },
+//     { 
+//         title: "Linux Fundamentals", 
+//         link: "https://drive.google.com/file/d/1d5K8jpTe4glHV__QU9VIcBlUaCsHWupy/view?usp=sharing", 
+//         thumbnail: "img/linuxfundamental.jpg"
+//     },
+//     { 
+//         title: "Mikrotik Kungfu Kitab 1", 
+//         link: "https://drive.google.com/file/d/177WNgCcn7Rd2VdEUN4iXiyPyIdk-CQq5/view?usp=sharing", 
+//         thumbnail: "img/mikrokitkungfu.jpg"
+//     },
+//     { 
+//         title: "Object Oriented Programming Using C#", 
+//         link: "https://drive.google.com/file/d/1YMSd_Tde6NY1Nfd1Yb9IRNzmLezLLKbM/view?usp=sharing", 
+//         thumbnail: "img/oopc.jpg"
+//     },
+//     { 
+//         title: "Peran Teknologi Dalam Pembangunan", 
+//         link: "https://drive.google.com/file/d/1nPJjOSB89XAw2PuHRe8hOf3VgXTkxI9d/view?usp=sharing", 
+//         thumbnail: "img/teknologimembangun.jpg"
+//     },
+//     { 
+//         title: "Dasar Pemrograman Rust", 
+//         link: "https://drive.google.com/file/d/1MJbtAVxZlcM4x_56GbzOH-4RZUxUvpdO/view?usp=sharing", 
+//         thumbnail: "img/dasarpemrogramanrust.jpg"
+//     },
+//     { 
+//         title: "Software Development, Design and Coding", 
+//         link: "https://drive.google.com/file/d/1W7-WexYbWpVFo1_gc9hsCJKbVu3aw4XH/view?usp=sharing", 
+//         thumbnail: "img/softwareandcoding.jpg"
+//     },
+//     { 
+//         title: "Deep Learning for Natural Language Processing", 
+//         link: "https://drive.google.com/file/d/1XxGvYDxf-092Ag1C0Y81nFsjLJyLAddz/view?usp=sharing", 
+//         thumbnail: "img/Deepnlp.jpg"
+//     },
+//     { 
+//         title: "Practical Machine Learning and Image Processing", 
+//         link: "https://drive.google.com/file/d/1tiD7P9QxNxZswRvjOxmBY8SpCpvSsTKD/view?usp=sharing", 
+//         thumbnail: "img/pmlandip.jpg"
+//     },
+//     { 
+//         title: "Java Server Pages Tutorial", 
+//         link: "https://drive.google.com/file/d/1e_N-YdX8P4maiqZa03fQUmKSdXorspNq/view?usp=sharing", 
+//         thumbnail: "img/javaservertutor.jpg"
+//     },
+//     { 
+//         title: "Cryptography", 
+//         link: "https://drive.google.com/file/d/1KMw-uepxSgQmvraS9AIHV0jY0o_BKukj/view?usp=sharing", 
+//         thumbnail: "img/cryptography.jpg"
+//     },
+//     { 
+//         title: "Boundary Values and Convolution in Ultradistribution Spaces", 
+//         link: "https://drive.google.com/file/d/1w3UixZfePjrzDwAA5ldtTyjYA4hO9ox2/view?usp=sharing", 
+//         thumbnail: "img/boundaryvalues.jpg"
+//     },
+//     { 
+//         title: "Islam dan Teknologi", 
+//         link: "https://drive.google.com/file/d/1mNXI0tOn_AcEELy7ua_wyUc34L_V4X8z/view?usp=sharing", 
+//         thumbnail: "img/islamteknologi.jpg"
+//     },
+//     { 
+//         title: " Linear Algebra", 
+//         link: "https://drive.google.com/file/d/1TmgkBO_hnHOXvGrVfBHiDvnNwXMyhetM/view?usp=sharing", 
+//         thumbnail: "img/algebra.jpg"
+//     },
+//     { 
+//         title: "Problems Based Course in Advanced Calculus", 
+//         link: "https://drive.google.com/file/d/1iG9oAcQwV99QH_aIOOcebrxTd5TGLVE4/view?usp=sharing", 
+//         thumbnail: "img/calculus.jpg"
+//     }
+
+    
+// ];
+
+// //fungsi tampilan buku
+// function renderBooks(booksToRender) {
+//     const container = document.getElementById("book-container");
+//     container.innerHTML = ""; // Bersihkan dulu
+
+//     if (booksToRender.length === 0) {
+//         container.innerHTML = `<p style="grid-column: 1 / -1; text-align: center; font-size: 18px; color: #555;">
+//             Maaf, buku belum tersedia.
+//         </p>`;
+//         return;
+//     }
+
+//     booksToRender.forEach(book => {
+//         const card = document.createElement("div");
+//         card.classList.add("book-card");
+//         card.innerHTML = `
+//             <a href="${book.link}" >
+//                 <img src="${book.thumbnail}" alt="${book.title}">
+//             </a>
+//             <h3>${book.title}</h3>
+//         `;
+//         container.appendChild(card);
+//     });
+// }
+
+// // Fungsi Pencarian
+// function setupSearch() {
+//     const searchInput = document.getElementById("searchInput");
+//     searchInput.addEventListener("input", () => {
+//         const query = searchInput.value.toLowerCase();
+//         const filteredBooks = books.filter(book => book.title.toLowerCase().includes(query));
+//         renderBooks(filteredBooks);
+//     });
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     renderBooks(books);
+//     setupSearch();
+// });
+
+
+// kode baru
+// ==================== Data Buku ====================
+// Pakai link Google Drive dengan format: /preview
 const books = [
+    {
+        title: "Jaringan Komputer",
+        link: "https://drive.google.com/file/d/1R1HLJrrW5B6YOegFDQTejZ20SFIedPfN/preview",
+        thumbnail: "img/jaringankomputer.jpg"
+    },
+    {
+        title: "Computer Graphics",
+        link: "https://drive.google.com/file/d/1XyzABCDefGhIJKLmnop/preview",
+        thumbnail: "img/Computergraphics.jpg"
+    },
     { 
         title: "Dasar Pemrograman Python", 
-        link: "https://drive.google.com/file/d/1R1HLJrrW5B6YOegFDQTejZ20SFIedPfN/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1R1HLJrrW5B6YOegFDQTejZ20SFIedPfN/preview", 
         thumbnail: "img/dasarpemrogramanpython.jpg"
     },
     { 
         title: "Computer Graphics", 
-        link: "https://drive.google.com/file/d/1pMHf74OUJv_0RAiLtqUlLUNVWejZm0kj/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1pMHf74OUJv_0RAiLtqUlLUNVWejZm0kj/preview", 
         thumbnail: "img/Computergraphics.jpg"
     },
     { 
         title: "Belajar MS WOrd 2012", 
-        link: "https://drive.google.com/file/d/1zWj3C3gSXtyy15IAo3W3vxQdZFpDWuxj/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1zWj3C3gSXtyy15IAo3W3vxQdZFpDWuxj/preview", 
         thumbnail: "img/belajarmsword2010.jpg"
     },
     { 
         title: "Panduan Merakit Komputer", 
-        link: "https://drive.google.com/file/d/1N12HOhNo5RLlBff6VH8n6q2Z0A6sXqLM/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1N12HOhNo5RLlBff6VH8n6q2Z0A6sXqLM/preview", 
         thumbnail: "img/bukumerakitkomputer.jpg"
     },
     { 
         title: "Analysis and Design Using MATLAB", 
-        link: "https://drive.google.com/file/d/16-wrtZjN27V0Taqvjg6xGG8PIIB4twKh/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/16-wrtZjN27V0Taqvjg6xGG8PIIB4twKh/preview", 
         thumbnail: "img/designmatlab.jpg"
     },
     { 
         title: "Bikin Game Anti Ribet", 
-        link: "https://drive.google.com/file/d/1XgRaa5lz68DpDLtHaaOJxuYEL4__69f-/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1XgRaa5lz68DpDLtHaaOJxuYEL4__69f-/preview", 
         thumbnail: "img/bikingameantiribet.jpg"
     },
     { 
         title: "Linux Fundamentals", 
-        link: "https://drive.google.com/file/d/1d5K8jpTe4glHV__QU9VIcBlUaCsHWupy/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1d5K8jpTe4glHV__QU9VIcBlUaCsHWupy/preview", 
         thumbnail: "img/linuxfundamental.jpg"
     },
     { 
         title: "Mikrotik Kungfu Kitab 1", 
-        link: "https://drive.google.com/file/d/177WNgCcn7Rd2VdEUN4iXiyPyIdk-CQq5/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/177WNgCcn7Rd2VdEUN4iXiyPyIdk-CQq5/preview", 
         thumbnail: "img/mikrokitkungfu.jpg"
     },
     { 
         title: "Object Oriented Programming Using C#", 
-        link: "https://drive.google.com/file/d/1YMSd_Tde6NY1Nfd1Yb9IRNzmLezLLKbM/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1YMSd_Tde6NY1Nfd1Yb9IRNzmLezLLKbM/preview", 
         thumbnail: "img/oopc.jpg"
     },
     { 
         title: "Peran Teknologi Dalam Pembangunan", 
-        link: "https://drive.google.com/file/d/1nPJjOSB89XAw2PuHRe8hOf3VgXTkxI9d/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1nPJjOSB89XAw2PuHRe8hOf3VgXTkxI9d/preview", 
         thumbnail: "img/teknologimembangun.jpg"
     },
     { 
         title: "Dasar Pemrograman Rust", 
-        link: "https://drive.google.com/file/d/1MJbtAVxZlcM4x_56GbzOH-4RZUxUvpdO/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1MJbtAVxZlcM4x_56GbzOH-4RZUxUvpdO/preview", 
         thumbnail: "img/dasarpemrogramanrust.jpg"
     },
     { 
         title: "Software Development, Design and Coding", 
-        link: "https://drive.google.com/file/d/1W7-WexYbWpVFo1_gc9hsCJKbVu3aw4XH/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1W7-WexYbWpVFo1_gc9hsCJKbVu3aw4XH/preview", 
         thumbnail: "img/softwareandcoding.jpg"
     },
     { 
         title: "Deep Learning for Natural Language Processing", 
-        link: "https://drive.google.com/file/d/1XxGvYDxf-092Ag1C0Y81nFsjLJyLAddz/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1XxGvYDxf-092Ag1C0Y81nFsjLJyLAddz/preview", 
         thumbnail: "img/Deepnlp.jpg"
     },
     { 
         title: "Practical Machine Learning and Image Processing", 
-        link: "https://drive.google.com/file/d/1tiD7P9QxNxZswRvjOxmBY8SpCpvSsTKD/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1tiD7P9QxNxZswRvjOxmBY8SpCpvSsTKD/preview", 
         thumbnail: "img/pmlandip.jpg"
     },
     { 
         title: "Java Server Pages Tutorial", 
-        link: "https://drive.google.com/file/d/1e_N-YdX8P4maiqZa03fQUmKSdXorspNq/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1e_N-YdX8P4maiqZa03fQUmKSdXorspNq/preview", 
         thumbnail: "img/javaservertutor.jpg"
     },
     { 
         title: "Cryptography", 
-        link: "https://drive.google.com/file/d/1KMw-uepxSgQmvraS9AIHV0jY0o_BKukj/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1KMw-uepxSgQmvraS9AIHV0jY0o_BKukj/preview", 
         thumbnail: "img/cryptography.jpg"
     },
     { 
         title: "Boundary Values and Convolution in Ultradistribution Spaces", 
-        link: "https://drive.google.com/file/d/1w3UixZfePjrzDwAA5ldtTyjYA4hO9ox2/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1w3UixZfePjrzDwAA5ldtTyjYA4hO9ox2/preview", 
         thumbnail: "img/boundaryvalues.jpg"
     },
     { 
         title: "Islam dan Teknologi", 
-        link: "https://drive.google.com/file/d/1mNXI0tOn_AcEELy7ua_wyUc34L_V4X8z/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1mNXI0tOn_AcEELy7ua_wyUc34L_V4X8z/preview", 
         thumbnail: "img/islamteknologi.jpg"
     },
     { 
         title: " Linear Algebra", 
-        link: "https://drive.google.com/file/d/1TmgkBO_hnHOXvGrVfBHiDvnNwXMyhetM/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1TmgkBO_hnHOXvGrVfBHiDvnNwXMyhetM/preview", 
         thumbnail: "img/algebra.jpg"
     },
     { 
         title: "Problems Based Course in Advanced Calculus", 
-        link: "https://drive.google.com/file/d/1iG9oAcQwV99QH_aIOOcebrxTd5TGLVE4/view?usp=sharing", 
+        link: "https://drive.google.com/file/d/1iG9oAcQwV99QH_aIOOcebrxTd5TGLVE4/preview", 
         thumbnail: "img/calculus.jpg"
     }
 
-    
+    // Tambahkan buku lain di sini...
 ];
 
-//fungsi tampilan buku
+// ==================== Render Daftar Buku ====================
 function renderBooks(booksToRender) {
     const container = document.getElementById("book-container");
-    container.innerHTML = ""; // Bersihkan dulu
+    container.innerHTML = "";
 
     if (booksToRender.length === 0) {
         container.innerHTML = `<p style="grid-column: 1 / -1; text-align: center; font-size: 18px; color: #555;">
@@ -120,16 +279,30 @@ function renderBooks(booksToRender) {
         const card = document.createElement("div");
         card.classList.add("book-card");
         card.innerHTML = `
-            <a href="${book.link}" >
-                <img src="${book.thumbnail}" alt="${book.title}">
-            </a>
+            <img src="${book.thumbnail}" alt="${book.title}">
             <h3>${book.title}</h3>
         `;
+        card.addEventListener("click", () => openPDF(book.link));
         container.appendChild(card);
     });
 }
 
-// Fungsi Pencarian
+// ==================== Buka & Tutup PDF ====================
+function openPDF(file) {
+    const overlay = document.getElementById("pdf-overlay");
+    const frame = document.getElementById("pdf-frame");
+    frame.src = file;
+    overlay.style.display = "block";
+}
+
+function closePDF() {
+    const overlay = document.getElementById("pdf-overlay");
+    const frame = document.getElementById("pdf-frame");
+    frame.src = ""; // reset biar berhenti load PDF
+    overlay.style.display = "none";
+}
+
+// ==================== Fungsi Pencarian ====================
 function setupSearch() {
     const searchInput = document.getElementById("searchInput");
     searchInput.addEventListener("input", () => {
@@ -139,6 +312,7 @@ function setupSearch() {
     });
 }
 
+// ==================== Init ====================
 document.addEventListener("DOMContentLoaded", () => {
     renderBooks(books);
     setupSearch();
